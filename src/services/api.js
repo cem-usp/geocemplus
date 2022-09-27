a618d757c1bf6ac8cabfd9a6b498af65220db529
Kaue Oliveira Almeida <kaue.oa@usp.br>
2022-09-27 12:29:43 -0300

    undo

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,5 @@ const protocol = (process.env.NODE_ENV == 'development') ? 'http' : 'https'
 const api_geocem = axios.create({
   baseURL: protocol+"://200.144.244.238/api",
 });
-console.log(process.env.NODE_ENV)
-console.log(protocol)
+
 export {axios, api_geocem};
