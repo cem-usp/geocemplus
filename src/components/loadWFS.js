cc2b87d868cccbdeb8bcd1aff8b6464f62f382b5
Kaue Oliveira Almeida <kaue.oa@usp.br>
2023-07-20 19:06:37 -0300

    imeplementing newretrieve layers structure

--- src/components/loadWFS.js
+++ src/components/loadWFS.js
@@ -1,9 +1,31 @@
 import {axios} from "../services/api";
 
-const wsf_servers = {
-    GeoCEM: 'https://geocem.centrodametropole.fflch.usp.br/geoserver/ows',
-    GeoSampa: 'http://wfs.geosampa.prefeitura.sp.gov.br/geoserver/geoportal/wfs'
-}
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import PublicIcon from '@mui/icons-material/Public';
+import ListItemText from '@mui/material/ListItemText';
+import LayersIcon from '@mui/icons-material/Layers';
+
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import Collapse from '@mui/material/Collapse';
+
+import React, { useEffect, useState } from 'react';
+
+const geoservices = [
+    {
+        name: 'GeoCEM',
+        catalog_baseurl: 'https://geocem.centrodametropole.fflch.usp.br/',
+        catalog_category_uri: '/api/categories/',
+        wfs_url: 'https://geocem.centrodametropole.fflch.usp.br/geoserver/ows'
+    },
+    {
+        name: 'GeoSampa',
+        wfs_url: 'http://wfs.geosampa.prefeitura.sp.gov.br/geoserver/geoportal/wfs'
+    }
+]
 
 const wfs_requests = {
     base: {
@@ -22,14 +44,92 @@ function requestWFS(wsf_server, request) {
     });
 }
 
-//Load WFS Servers
-useEffect(() => {
-    
-})
 
-export default function layerSelection(props) {
+export default function LayerList(props) {
+    
+    const [open, setOpen] = useState([]);
+    const [layers_lists, setLists] = useState('')
+    
+    const [geocem_cats, setGeoCEMCats] = useState([])
+    const [geocem_cats_list, setGeoCEMCatsList] = useState('')
+
+    function handleClick(id) {
+        let newopen = []
+        newopen[id] = !open[id]
+        console.log(newopen)
+        setOpen(newopen)
+    }
+
+
+    useEffect(() => {
+        loadGeoCEMCategories()
+    }, [])
+
+    useEffect(() => {
+        createCatList()
+    }, [geocem_cats])
+
+    function loadGeoCEMCategories() {
+        const request = axios.create({
+            baseURL: geoservices[0].catalog_baseurl,
+            params: {type: 'layer'},
+        });
+
+        request.get(geoservices[0].catalog_category_uri)
+            .then((response) => {
+                setGeoCEMCats(response.data.objects)
+            })
+    }
+
+    function createCatList() {
+        setGeoCEMCatsList (geocem_cats.map(category => 
+            <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                    <LayersIcon />
+                </ListItemIcon>
+                <ListItemText primary={category.gn_description} />
+            </ListItemButton>
+            
+        ));
+    }
+
+    const geoservices_list = geoservices.map(function (geoservice, i) {        
+        return (
+            <div key={geoservice.name}>
+
+                <ListItemButton onClick={() => handleClick(geoservice.name)}>
+                    <ListItemIcon>
+                        <PublicIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={geoservice.name} />
+                    {open[geoservice.name] ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+
+                <Collapse in={open[geoservice.name]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        {geocem_cats_list}
+                    </List>
+                </Collapse>
+
+            </div>
+
+        )
+    })
+
     return (
-        <div>
-        </div>
-    )
+        <List
+            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                Bases de dados Cartográficas
+                </ListSubheader>
+            }
+        >
+
+            {geoservices_list}
+
+        </List>
+      )
 }
