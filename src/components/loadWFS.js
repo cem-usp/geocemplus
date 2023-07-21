87d5c801a4f3e9103b31d24d635437f49ba6c45d
Kaue Oliveira Almeida <kaue.oa@usp.br>
2023-07-21 15:57:01 -0300

    implementing new layer selection

--- src/components/loadWFS.js
+++ src/components/loadWFS.js
@@ -7,6 +7,7 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import PublicIcon from '@mui/icons-material/Public';
 import ListItemText from '@mui/material/ListItemText';
 import LayersIcon from '@mui/icons-material/Layers';
+import AddIcon from '@mui/icons-material/Add';
 
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
@@ -17,8 +18,9 @@ import React, { useEffect, useState } from 'react';
 const geoservices = [
     {
         name: 'GeoCEM',
-        catalog_baseurl: 'https://geocem.centrodametropole.fflch.usp.br/',
-        catalog_category_uri: '/api/categories/',
+        baseurl: 'https://geocem.centrodametropole.fflch.usp.br/api/',
+        catalog_category_uri: 'categories/',
+        catolog_layers_in_cat_uri: '/layers/?category__identifier__in=',
         wfs_url: 'https://geocem.centrodametropole.fflch.usp.br/geoserver/ows'
     },
     {
@@ -48,6 +50,8 @@ function requestWFS(wsf_server, request) {
 export default function LayerList(props) {
     
     const [open, setOpen] = useState([]);
+    const [openCat, setOpenCat] = useState([]);
+
     const [layers_lists, setLists] = useState('')
     
     const [geocem_cats, setGeoCEMCats] = useState([])
@@ -60,6 +64,13 @@ export default function LayerList(props) {
         setOpen(newopen)
     }
 
+    function handleClickCat(id) {
+        let newopen = []
+        newopen[id] = !openCat[id]
+        console.log(newopen)
+        setOpenCat(newopen)
+    }
+
 
     useEffect(() => {
         loadGeoCEMCategories()
@@ -67,45 +78,87 @@ export default function LayerList(props) {
 
     useEffect(() => {
         createCatList()
-    }, [geocem_cats])
+    }, [geocem_cats, openCat])
 
     function loadGeoCEMCategories() {
         const request = axios.create({
-            baseURL: geoservices[0].catalog_baseurl,
+            baseURL: geoservices[0].baseurl,
             params: {type: 'layer'},
-        });
-
+        });        
+        
         request.get(geoservices[0].catalog_category_uri)
             .then((response) => {
-                setGeoCEMCats(response.data.objects)
+                let promises = []
+                response.data.objects.forEach(category => {
+                    if(category.layers_count > 0 ) {
+                        const promise = request
+                            .get(geoservices[0].catolog_layers_in_cat_uri+category.identifier)
+                            .then((reponsecat) => {
+                                const new_category = {
+                                    name: category.gn_description,
+                                    id: category.id,
+                                    layers: reponsecat.data.objects
+                                }
+                                return new_category
+                            })
+                        promises.push(promise)
+                    }
+                });
+                return promises
+            })
+            .then((promises) => {
+                Promise.all(promises)
+                .then((categories) => setGeoCEMCats(categories));
             })
     }
 
     function createCatList() {
-        setGeoCEMCatsList (geocem_cats.map(category => 
-            <ListItemButton sx={{ pl: 4 }}>
-                <ListItemIcon>
-                    <LayersIcon />
-                </ListItemIcon>
-                <ListItemText primary={category.gn_description} />
-            </ListItemButton>
+       const geocem_list = geocem_cats.map((category) =>
+       { 
+            const layers = category.layers.map(layer => 
+                <ListItemButton sx={{ pl: 8 }}>
+                    <ListItemIcon>
+                        <LayersIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={layer.title} />
+                </ListItemButton>)
+
+            return (
+                <div>
+                    <ListItemButton sx={{ pl: 4 }}  onClick={() => handleClickCat('cat_'+category.id)}>
+                        <ListItemIcon>
+                            <AddIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={category.name} />
+                        {openCat['cat_'+category.id] ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+
+                    <Collapse in={openCat['cat_'+category.id]} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            {layers}
+                        </List>
+                    </Collapse>    
+                </div>
+            )
             
-        ));
+        });
+
+        setGeoCEMCatsList(geocem_list)
     }
 
-    const geoservices_list = geoservices.map(function (geoservice, i) {        
-        return (
-            <div key={geoservice.name}>
+    const geoservices_list =       
+        
+            <div key={'gs_'+geoservices[0].name}>
 
-                <ListItemButton onClick={() => handleClick(geoservice.name)}>
+                <ListItemButton onClick={() => handleClick('gs_'+geoservices[0].name)}>
                     <ListItemIcon>
                         <PublicIcon />
                     </ListItemIcon>
-                    <ListItemText primary={geoservice.name} />
-                    {open[geoservice.name] ? <ExpandLess /> : <ExpandMore />}
+                    <ListItemText primary={geoservices[0].name} />
+                    {open['gs_'+geoservices[0].name] ? <ExpandLess /> : <ExpandMore />}
                 </ListItemButton>
 
-                <Collapse in={open[geoservice.name]} timeout="auto" unmountOnExit>
+                <Collapse in={open['gs_'+geoservices[0].name]} timeout="auto" unmountOnExit>
                     <List component="div" disablePadding>
                         {geocem_cats_list}
                     </List>
@@ -113,8 +166,8 @@ export default function LayerList(props) {
 
             </div>
 
-        )
-    })
+        
+    
 
     return (
         <List
